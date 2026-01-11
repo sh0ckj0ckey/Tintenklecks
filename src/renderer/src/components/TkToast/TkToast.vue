0b8e865c319ed6be9fbb5bd8df2c2faccd324ab5
姚一鸣 <yaoyiming123@live.com>
2026-01-11 18:54:29 +0800

    Remove unnecessary align-self properties in TkToast styles

--- src/renderer/src/components/TkToast/TkToast.vue
+++ src/renderer/src/components/TkToast/TkToast.vue
@@ -29,7 +29,9 @@ const props = defineProps<{
   offset: number | string
 }>()
 
-const emit = defineEmits(['close'])
+const emit = defineEmits<{
+  (e: 'close'): void
+}>()
 
 const iconPaths = {
   info: 'M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0Zm-7-4a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM9 9a.75.75 0 0 0 0 1.5h.253a.25.25 0 0 1 .244.304l-.459 2.066A1.75 1.75 0 0 0 10.747 15H11a.75.75 0 0 0 0-1.5h-.253a.25.25 0 0 1-.244-.304l.459-2.066A1.75 1.75 0 0 0 9.253 9H9Z',
@@ -77,17 +79,14 @@ onBeforeUnmount(() => {
   pointer-events: none;
 
   &.placement-top {
-    align-self: flex-start;
     margin-top: v-bind(offsetValue);
   }
 
   &.placement-center {
-    align-self: center;
     margin-top: v-bind(offsetValue);
   }
 
   &.placement-bottom {
-    align-self: flex-end;
     margin-bottom: v-bind(offsetValue);
   }
 
