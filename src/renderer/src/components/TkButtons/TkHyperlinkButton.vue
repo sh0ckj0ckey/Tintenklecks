f9efe4b9a00ce726996fc2e0eacf63f9cf948362
姚一鸣 <yaoyiming@huya.com>
2026-01-13 20:04:59 +0800

    Update TkHyperlinkButton styles (WIP)

--- src/renderer/src/components/TkButtons/TkHyperlinkButton.vue
+++ src/renderer/src/components/TkButtons/TkHyperlinkButton.vue
@@ -1,23 +1,11 @@
 <template>
-  <button
-    class="tk-hyperlink-button"
-    :class="{ emboss: props.emboss }"
-    :style="style"
-    :disabled="props.disabled"
-    type="button"
-    @click="handleClick"
-  >
+  <button type="button" class="tk-hyperlink-button" :class="{ emboss: props.emboss }" :disabled="props.disabled" @click="onClick">
     <slot></slot>
   </button>
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue'
-
 const props = defineProps<{
-  borderRadius?: string | number
-  height?: string | number
-  width?: string | number
   emboss?: boolean
   disabled?: boolean
 }>()
@@ -26,13 +14,7 @@ const emit = defineEmits<{
   (e: 'click', event: MouseEvent): void
 }>()
 
-const style = computed(() => ({
-  borderRadius: props.borderRadius ? (typeof props.borderRadius === 'number' ? `${props.borderRadius}px` : props.borderRadius) : undefined,
-  height: props.height ? (typeof props.height === 'number' ? `${props.height}px` : props.height) : undefined,
-  width: props.width ? (typeof props.width === 'number' ? `${props.width}px` : props.width) : undefined
-}))
-
-const handleClick = (event: MouseEvent): void => {
+const onClick = (event: MouseEvent): void => {
   if (!props.disabled) {
     emit('click', event)
   }
@@ -44,28 +26,20 @@ const handleClick = (event: MouseEvent): void => {
   display: inline-flex;
   align-items: center;
   justify-content: center;
-  background-color: transparent;
+  padding: 8px 16px;
   border: none;
-  padding: 0;
-  opacity: 1;
+  border-radius: 4px;
+  background-color: transparent;
+  font-size: 16px;
+  font-weight: normal;
   cursor: pointer;
-  user-select: none;
-  transition:
-    filter 0.2s,
-    opacity 0.2s,
-    background-color 0.2s;
 
   &.emboss {
-    background-color: #88888810;
-    box-shadow:
-      inset 1px 1px 2px #ffffff,
-      inset -1px -1px 2px #888888;
+    text-shadow: 0px 1px 0px rgba(0, 0, 0, 0.5);
   }
 
   &:hover:not(:disabled) {
     background-color: #88888828;
-    filter: brightness(1.1);
-    opacity: 1;
   }
 
   &:active:not(:disabled) {
@@ -75,7 +49,7 @@ const handleClick = (event: MouseEvent): void => {
   }
 
   &:focus-visible {
-    outline: 2px solid #88888880;
+    outline: 2px solid currentColor;
     outline-offset: 2px;
   }
 }
