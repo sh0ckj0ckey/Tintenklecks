0fc5b44d67589d1269d7ecd653c0ee8ed8207127
姚一鸣 <yaoyiming@huya.com>
2026-01-14 17:58:11 +0800

    Add TkToggleButton (WIP)

---
+++ src/renderer/src/components/TkButtons/TkToggleButton.vue
@@ -0,0 +1,90 @@
+<template>
+  <button type="button" class="tk-toggle-button" :class="{ emboss: props.emboss }" :disabled="props.disabled" @click="onClick">
+    <span class="tk-toggle-button-content">
+      <slot></slot>
+    </span>
+  </button>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  emboss?: boolean
+  disabled?: boolean
+}>()
+
+const emit = defineEmits<{
+  (e: 'click', event: MouseEvent): void
+}>()
+
+const onClick = (event: MouseEvent): void => {
+  if (!props.disabled) {
+    emit('click', event)
+  }
+}
+</script>
+
+<style scoped>
+.tk-toggle-button {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  padding: 8px 16px;
+  background-color: transparent;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  font-size: 14px;
+  font-weight: normal;
+  color: #0078d4;
+  cursor: pointer;
+  transition:
+    transform 0.1s,
+    background-color 0.2s,
+    color 0.2s,
+    box-shadow 0.2s;
+
+  .tk-toggle-button-content {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: inherit;
+    justify-content: inherit;
+    transition: filter 0.2s;
+  }
+
+  &:disabled {
+    opacity: 0.6;
+  }
+
+  &.emboss .tk-toggle-button-content {
+    filter: drop-shadow(-1px -1px 0px rgba(255, 255, 255, 0.08)) drop-shadow(1px 1px 0px rgba(0, 0, 0, 0.6));
+  }
+
+  &:hover:not(:disabled) {
+    background-color: rgba(255, 255, 255, 0.08);
+    color: #ffffffe6;
+    box-shadow:
+      0 2px 4px rgba(0, 0, 0, 0.2),
+      inset 0 1px 0 rgba(255, 255, 255, 0.08),
+      inset 0 -1px 0 rgba(255, 255, 255, 0.03);
+
+    &.emboss .tk-toggle-button-content {
+      filter: drop-shadow(1px 1px 0px rgba(255, 255, 255, 0.15)) drop-shadow(-1px -1px 0px rgba(0, 0, 0, 0.6));
+    }
+  }
+
+  &:active:not(:disabled) {
+    background-color: rgba(255, 255, 255, 0.05);
+    color: #ffffffb3;
+    transform: translateY(1px);
+    box-shadow:
+      0 1px 2px rgba(0, 0, 0, 0.2),
+      inset 0 1px 0 rgba(255, 255, 255, 0.08),
+      inset 0 -1px 0 rgba(255, 255, 255, 0.03);
+  }
+
+  &:focus-visible {
+    outline: 2px dashed currentColor;
+    outline-offset: 2px;
+  }
+}
+</style>
