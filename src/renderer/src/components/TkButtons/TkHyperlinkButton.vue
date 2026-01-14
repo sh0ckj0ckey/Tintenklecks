e770e54063a20470e1a7c27bc6f80d81aeed428d
姚一鸣 <yaoyiming@huya.com>
2026-01-14 17:55:29 +0800

    Refactor TkHyperlinkButton styles for improved layout and consistency

--- src/renderer/src/components/TkButtons/TkHyperlinkButton.vue
+++ src/renderer/src/components/TkButtons/TkHyperlinkButton.vue
@@ -40,18 +40,22 @@ const onClick = (event: MouseEvent): void => {
     transform 0.1s,
     background-color 0.2s,
     color 0.2s,
-    box-shadow 0.2s,
-    filter 0.2s;
+    box-shadow 0.2s;
 
   .tk-hyperlink-button-content {
-    display: contents;
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: inherit;
+    justify-content: inherit;
+    transition: filter 0.2s;
   }
 
   &:disabled {
     opacity: 0.6;
   }
 
-  &.emboss {
+  &.emboss .tk-hyperlink-button-content {
     filter: drop-shadow(-1px -1px 0px rgba(255, 255, 255, 0.08)) drop-shadow(1px 1px 0px rgba(0, 0, 0, 0.6));
   }
 
@@ -63,15 +67,15 @@ const onClick = (event: MouseEvent): void => {
       inset 0 1px 0 rgba(255, 255, 255, 0.08),
       inset 0 -1px 0 rgba(255, 255, 255, 0.03);
 
-    &.emboss {
+    &.emboss .tk-hyperlink-button-content {
       filter: drop-shadow(1px 1px 0px rgba(255, 255, 255, 0.15)) drop-shadow(-1px -1px 0px rgba(0, 0, 0, 0.6));
     }
   }
 
   &:active:not(:disabled) {
-    transform: translateY(1px);
     background-color: rgba(255, 255, 255, 0.05);
     color: #ffffffb3;
+    transform: translateY(1px);
     box-shadow:
       0 1px 2px rgba(0, 0, 0, 0.2),
       inset 0 1px 0 rgba(255, 255, 255, 0.08),
