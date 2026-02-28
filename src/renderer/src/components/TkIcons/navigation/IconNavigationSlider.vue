547b15f1c75e2326142cf4592b69519428c9c40f
sh0ckj0ckey <yaoyiming123@live.com>
2026-02-28 16:56:13 +0800

    Add IconNavigationSlider SVG icon

---
+++ src/renderer/src/components/TkIcons/navigation/IconNavigationSlider.vue
@@ -0,0 +1,7 @@
+<template>
+  <svg viewBox="0 0 64 42" xmlns="http://www.w3.org/2000/svg" fill="none">
+    <line x1="8" y1="21" x2="18" y2="21" stroke="currentColor" stroke-width="4" stroke-linecap="round" />
+    <circle cx="26" cy="21" r="6" stroke="currentColor" stroke-width="4" />
+    <line x1="34" y1="21" x2="56" y2="21" stroke="currentColor" stroke-width="4" stroke-linecap="round" />
+  </svg>
+</template>
