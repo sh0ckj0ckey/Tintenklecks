2ab383c6b6849b97bcf532bd25740448173116de
姚一鸣 <yaoyiming123@live.com>
2026-08-23 23:09:40 +0800

    Remove test code in index.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -104,9 +104,6 @@ app.whenReady().then(() => {
     optimizer.watchWindowShortcuts(window)
   })
 
-  // IPC test
-  ipcMain.on('ping', () => console.log('pong'))
-
   createWindow()
 
   app.on('activate', function () {
